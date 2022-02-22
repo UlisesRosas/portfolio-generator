abe6db3ae7860d0525b42f55ea0f2193d57a2cbf
ulisMac <rosas_u@ymail.com>
2022-02-21 21:21:21 -0800

    addded array for multiple sets of responses

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+
 // // required statement to be able to use file system module "fs.". requires is a built in functio available in node.js
 // const fs = require('fs');
 // // alos allows us to inport the module we made in page-template.js file
@@ -16,7 +17,7 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is yoyr name?'
+            message: 'What is your name?'
         },
         {
             type: 'input',
@@ -28,52 +29,72 @@ const promptUser = () => {
             name: 'about',
             message: 'Provide some information about yourself:'
         }
+        
     ]);
 };
-promptUser()
-.then(answers => console.log(answers))
-.then(promptProject)
-.then(projectAnswers => console.log(projectAnswers));
 
-const promptProject = () => {
+
+
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects){
+        portfolioData.projects = [];
+    }
+
     console.log(`
-    =================
-    Add a New Project
-    =================
-    `);
+  =================
+  Add a New Project
+  =================
+  `);
     return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'description',
-            message: 'What is the name of your project'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of the project (Required)'
-        },
-        {
-            type: 'checkbox',
-            name: 'languages',
-            message: 'What did you build this project with? (Check all that apply)',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-        },
-        {
-            type: 'input',
-            name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
-        },
-        {
-            type: 'confirm',
-            name: 'feature',
-            message: 'Would you like to feature this project?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAddProject',
-            message: 'Would you like to enter another project?',
-            default: false
-        }     
-    ]);
-};
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    
+    ]) .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject){
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+      });
+
+    
+  }; 
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  console.log(portfolioData);
+});
+
