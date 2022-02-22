367370c7bb95f6528eb356b6272a6706e805fe95
ulisMac <rosas_u@ymail.com>
2022-02-21 20:16:33 -0800

    added inquirer as dependense and downloaded the node modules

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-// required statement to be able to use file system module "fs.". requires is a built in functio available in node.js
-const fs = require('fs');
-// alos allows us to inport the module we made in page-template.js file
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// // required statement to be able to use file system module "fs.". requires is a built in functio available in node.js
+// const fs = require('fs');
+// // alos allows us to inport the module we made in page-template.js file
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2);
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-// this is the same as the two variables above these brackets specify which item in the array the value will be based on position of the variabe
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+//     console.log('Portolio complete! Check out index.html to see the output!');
+// });
 
-// file sistem module from node taking three perams
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portolio complete! Check out index.html to see the output!');
-});
-
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is yoyr name?'
+        }
+    ])
+    .then(answers => console.log(answers));
